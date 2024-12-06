cb266d9921ca96be672fd005ec60897d6fb2650c
ShabalinaYulia <julia07.kurash@gmail.com>
2024-12-07 00:09:15 +0500

    Task5

--- Tyuiu.ShabalinaYP.Sprint6.Task5.V29/Form1.cs
+++ Tyuiu.ShabalinaYP.Sprint6.Task5.V29/Form1.cs
@@ -1,8 +1,8 @@
 namespace Tyuiu.ShabalinaYP.Sprint6.Task5.V29
 {
-    public partial class FormMain : Form
+    public partial class Form1 : Form
     {
-        public FormMain()
+        public Form1()
         {
             InitializeComponent();
         }
